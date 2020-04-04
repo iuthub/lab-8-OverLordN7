2ff897f3177469365648590874608a34f45e669e
OverLordN7 <arifdjanovsarvar@gmail.com>
2020-04-04 17:28:19 +0500

    done

---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('content')
+    <head>
+        @include('partials/header');
+    </head>
+    <div>
+        <p>
+            Lorem Overlord .....
+        </p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -12,5 +13,36 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('blog/index');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/',function (){
+        return view('admin/index');
+    });
+    Route::get('admin/create',function (){
+        return view('admin/create');
+    });
+    Route::get('admin/edit/{id}',function ($id){
+        return view('admin/edit');
+    });
+
+});
+Route::get("/about",function (){{
+    return view('other/about');
+}});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', [
+        'uses' => 'PostController@getAdminIndex',
+        'as' => 'admin.index'
+    ]);
+});
+Route::get('/', [
+    'uses' => 'PostController@getIndex',
+    'as' => 'blog.index'
+]);
+
+Route::get('post/{id}', [
+    'uses' => 'PostController@getPost',
+    'as' => 'blog.post'
+]);
